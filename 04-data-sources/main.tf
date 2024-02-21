d783d533daef000131f1cfc8902b1d8dfb99a2b0
Deepthi <dpdevops6@gmail.com>
2024-02-21 18:13:46 -0500

    Terraform

--- 04-data-sources/main.tf
+++ 04-data-sources/main.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "ami" {
+data "aws_ami_ids" "ami" {
   most_recent = true
   name_regex  = "Centos-8*"
   owners      = ["973714476881"]
 }
 
 output "ami_id" {
-  value = "data.aws_ami.ami"
+  value = "data.aws_ami_ids.ami"
 }
