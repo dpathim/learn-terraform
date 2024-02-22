193bdedd8b7526378a1b5387f24aa95e61e340ac
Deepthi <dpdevops6@gmail.com>
2024-02-22 13:26:23 -0500

    Terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -46,16 +46,16 @@ resource "aws_instance" "instance" {
   }
 }
 
-#resource "aws_route53_record" "record" {
-#  for_each = var.components
-#  zone_id = var.zone_id
-#  name    = "${lookup(each.value, "name", null)}.vdevops562.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.instance.private_ip]
-#}
-
-output "instances" {
-  value = aws_instance.instance
+resource "aws_route53_record" "record" {
+  for_each = var.components
+  zone_id = var.zone_id
+  name    = "${lookup(each.value, "name", null)}.vdevops562.online"
+  type    = "A"
+  ttl     = 30
+  records = [ lookup(lookup(aws_instance.instance,each.key,null),"private_ip", null) ]
 }
+
+#output "instances" {
+#  value = aws_instance.instance
+#}
 
