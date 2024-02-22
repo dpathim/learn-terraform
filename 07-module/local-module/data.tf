cd05de8f0534361720a53fa2e316713407113369
Deepthi <dpdevops6@gmail.com>
2024-02-22 14:54:35 -0500

    Terraform

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,8 +1,20 @@
-data "aws_ami_ids" "ami" {
-  name_regex  = "Centos-8*"
+data "aws_ami" "ami" {
+  most_recent =true
+  name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
+
+
+
+resource "aws_instance" "instance" {
+  ami           = "data.aws_ami.ami.id"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-038da0861b4c1788f"]
+
+}
+
+#variable = "instance_type"{}
