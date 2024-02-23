b98790605ca4370870e751cd84d3f936f4e3afeb
Deepthi <dpdevops6@gmail.com>
2024-02-22 19:48:34 -0500

    Terraform

--- 09-state/main.tf
+++ 09-state/main.tf
@@ -1,7 +1,9 @@
 terraform {
-  backend "s3" {bucket = "tf-statefile-v562"
+  backend "s3" {
+    bucket = "tf-statefile-v562"
     key    = "09-state/terraform.tf.state"
-    region = "us-east-1"}
+    region = "us-east-1"
+  }
 }
 
 variable "test" {
