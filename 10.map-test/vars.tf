722cb026d5b68c4b7b38334a7231b499f6e1bc55
Deepthi <dpdevops6@gmail.com>
2024-02-29 18:09:40 -0500

    Terraform

--- 10.map-test/vars.tf
+++ 10.map-test/vars.tf
@@ -31,9 +31,9 @@ variable "vpc" {
 #}
 
 locals {
-  public = { for k,v in merge(var.vpc["main"]["subnets"],["public"]): k => v }
-  app = { for k,v in merge(var.vpc["main"]["subnets"],["app"]): k => v }
-  db = { for k,v in merge(var.vpc["main"]["subnets"],["db"]): k => v }
+  public = { for k,v in merge(var.vpc["main"]["subnets"]["public"]): k => v }
+  app = { for k,v in merge(var.vpc["main"]["subnets"]["app"]): k => v }
+  db = { for k,v in merge(var.vpc["main"]["subnets"]["db"]): k => v }
 }
 
 output "all_subnets_nested_map" {
